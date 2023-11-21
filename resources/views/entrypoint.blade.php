b38cba4e7325e426ae452a699b50e7e93ebce64f
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:31:08 +0000

    feat: update app icon and title

--- resources/views/entrypoint.blade.php
+++ resources/views/entrypoint.blade.php
@@ -1,9 +1,11 @@
-<html class="h-full bg-gray-100">
+<html>
 <head>
+    <title>WindWise</title>
+    <link rel="icon" href="https://thecyberhawk.com/wp-content/uploads/2023/09/Cyberhawk-icon_black_yellow.png">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
-<body class="h-full">
+<body>
     <div id="app"></div>
 </body>
 <script src="{{ mix('/js/app.js') }}"></script>
