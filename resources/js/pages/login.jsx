33e2c3036c3b24c09b3cc4b5a7a6b96ab84dfca5
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:26:53 +0000

    feat: add placeholder for login page

---
+++ resources/js/pages/login.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () => {
+        // Add your login logic here
+        console.log(`Logging in with email: ${email}, password: ${password}`);
+    };
+
+    return (
+        <div className="flex h-full items-center justify-center bg-background">
+            <div className="p-8 rounded shadow-md w-96">
+                <h2 className="text-2xl font-semibold mb-6 text-text">
+                    Log In
+                </h2>
+                <form>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="email"
+                            className="block text-sm font-medium text-text"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-primary"
+                            placeholder="example@example.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-medium text-text"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-primary"
+                            placeholder="********"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="text-center">
+                        <button
+                            type="button"
+                            className="bg-background text-primary px-8 py-2 rounded-2xl hover:text-background hover:bg-primary border-primary border-2"
+                            onClick={handleLogin}
+                        >
+                            Log In
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
