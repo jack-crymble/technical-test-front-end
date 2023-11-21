9296363b95a3e3dd8a2d22a3c4e14563e3aaec9e
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:28:27 +0000

    feat: add custom colour palate

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
         "./resources/**/*.js",
         "./resources/**/*.jsx",
     ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFC72C",
+                background: "#14142A",
+                secondary: "#23263A",
+                accent: "#2C64FF",
+                error: "#E74C3C",
+                text: "#D8DBE9",
+            },
+        },
+        letterSpacing: {
+            widest: ".15em",
+        },
+    },
+    plugins: [],
+};
