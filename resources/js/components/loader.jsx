a6b1fce1523d4c0c83a7c6c6b6be3dbe824c7e48
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:25:50 +0000

    feat: add custom loaded and styles

---
+++ resources/js/components/loader.jsx
@@ -0,0 +1,17 @@
+import "./loader.css";
+export default function Loader() {
+    return (
+        <div className="wrapper">
+            <div className="lds-roller">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+}
