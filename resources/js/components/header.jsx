719be0d15c125856c7a7daa6b3d02446968ebc97
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:26:00 +0000

    feat: add header

---
+++ resources/js/components/header.jsx
@@ -0,0 +1,69 @@
+import { faHome, faUser, faWind } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+    const navigate = useNavigate();
+
+    const [userDropdownOpen, setDropdown] = useState(false);
+
+    const handleNavigation = () => navigate("/dashboard");
+
+    const handleUser = () => {
+        setDropdown(!userDropdownOpen);
+    };
+
+    return (
+        <header className="flex justify-between items-center px-8 py-4 bg-secondary">
+            <button
+                className="rounded-xl p-4 text-xl bg-secondary text-primary hover:bg-primary hover:text-secondary"
+                onClick={handleNavigation}
+            >
+                <FontAwesomeIcon icon={faHome} />
+            </button>
+            <h1
+                className="flex gap-2 items-center text-3xl tracking-widest cursor-pointer text-text"
+                onClick={handleNavigation}
+            >
+                <span>WindWise</span>
+                <FontAwesomeIcon icon={faWind} />
+            </h1>
+            <button
+                className="rounded-xl p-4 text-xl bg-secondary text-primary hover:bg-primary hover:text-secondary"
+                onClick={handleUser}
+                data-dropdown-toggle="dropdown"
+            >
+                <FontAwesomeIcon icon={faUser} />
+                {userDropdownOpen && (
+                    <div
+                        id="dropdown"
+                        className="absolute top-20 right-7 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                    >
+                        <ul
+                            className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="dropdownDefaultButton"
+                        >
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    Sign in
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    Settings
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </button>
+        </header>
+    );
+}
