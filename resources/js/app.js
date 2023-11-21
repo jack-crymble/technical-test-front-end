1dc3e69839f79f8f4d7427e47f3a43e893732053
Jack Crymble <jack.crymble@gmail.com>
2023-11-21 22:29:07 +0000

    feat: update base of application with routing

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,50 @@
-import App from './components/App'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import Header from "./components/header";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import InspectionPage from "./pages/inspection";
+import Dashboard from "./pages/dashboard";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Login from "./pages/login";
 
-const container = document.getElementById('app');
+export default function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <main className="flex flex-col p-8 bg-background h-[calc(100vh-88px)] overflow-auto">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Navigate replace to="/login" />}
+                        />
+                        <Route
+                            path="*"
+                            element={<Navigate replace to="/login" />}
+                        />
+                        <Route
+                            exact
+                            path="/dashboard"
+                            element={<Dashboard />}
+                        />
+                        <Route
+                            exact
+                            path="/inspection"
+                            element={<InspectionPage />}
+                        />
+                        <Route exact path="/login" element={<Login />} />
+                    </Routes>
+                </main>
+            </BrowserRouter>
+        </>
+    );
+}
+
+const container = document.getElementById("app");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
