d27c67ec466ee9b9c73da6a13fbdff0d13047d28
Jack Crymble <jack.crymble@gmail.com>
2023-11-22 21:21:41 +0000

    feat: add number display component

---
+++ resources/js/components/number-display.jsx
@@ -0,0 +1,34 @@
+import Error from "../pages/error";
+import NumberDisplayAverage from "./number-display-average";
+
+export default function NumberDisplay({
+    title,
+    aggregate = "avg",
+    values = [],
+}) {
+    let componentToRender;
+
+    switch (aggregate) {
+        case "avg":
+            componentToRender = <NumberDisplayAverage values={values} />;
+            break;
+        case "sum":
+            // Can be added in future
+            // componentToRender = <NumberDisplaySum values={values} />;
+            break;
+        case "min":
+            // Can be added in future
+            // componentToRender = <NumberDisplayMin values={values} />;
+            break;
+        default:
+            console.error(`Unable to calculate ${aggregate} of ${values}`);
+            return <Error className="text-center" />;
+    }
+
+    return (
+        <>
+            <p>{title}</p>
+            {componentToRender}
+        </>
+    );
+}
