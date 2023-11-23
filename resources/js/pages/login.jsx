05059ef8b05ef35422c643dd1d6d1b57832f64e6
Jack Crymble <jack.crymble@gmail.com>
2023-11-23 23:08:16 +0000

    feat: add redirect on login

--- resources/js/pages/login.jsx
+++ resources/js/pages/login.jsx
@@ -1,49 +1,56 @@
 import { useState } from "react";
 import Button from "../components/button";
+import { useDispatch } from "react-redux";
+import { login } from "../store/features/auth-slice";
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("");
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = () => {
-        // Add your login logic here
-        console.log(`Logging in with email: ${email}, password: ${password}`);
+        dispatch(
+            login({
+                user: username,
+            })
+        );
     };
 
     return (
         <div className="flex h-full items-center justify-center bg-background">
-            <div className="p-8 rounded shadow-md w-96">
+            <div className="w-96">
                 <h2 className="text-2xl font-semibold mb-6 text-text">
                     Log In
                 </h2>
                 <form>
                     <div className="mb-6">
                         <label
-                            htmlFor="email"
-                            className="block text-sm font-medium text-text"
+                            htmlFor="username"
+                            className="text-sm font-medium"
                         >
-                            Email
+                            Username
                         </label>
                         <input
-                            type="email"
-                            id="email"
-                            className="mt-1 p-2 w-full border rounded-md"
-                            placeholder="example@example.com"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                            id="username"
+                            className="mt-1 p-2 w-full border rounded-md text-black"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                         />
                     </div>
                     <div className="mb-6">
                         <label
                             htmlFor="password"
-                            className="block text-sm font-medium text-text"
+                            className="block text-sm font-medium"
                         >
                             Password
                         </label>
                         <input
                             type="password"
                             id="password"
-                            className="mt-1 p-2 w-full border rounded-md"
+                            className="mt-1 p-2 w-full border rounded-md text-black"
                             placeholder="********"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
