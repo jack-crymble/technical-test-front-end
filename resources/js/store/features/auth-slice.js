95562a9dc8e44e7bf934e0a3e07d0ba34530c3bf
Jack Crymble <jack.crymble@gmail.com>
2023-11-23 23:07:19 +0000

    feat: add actions to auth slice

--- resources/js/store/features/auth-slice.js
+++ resources/js/store/features/auth-slice.js
@@ -2,15 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     authenticated: false,
-    authenticating: false,
+    user: null,
 };
 
 export const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers: {},
+    reducers: {
+        login: (state, action) => ({
+            ...state,
+            authenticated: true,
+            user: action.payload.user,
+        }),
+        logout: (state) => ({
+            ...state,
+            authenticated: false,
+            user: null,
+        }),
+    },
 });
 
-// export const {} = authSlice.actions
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
